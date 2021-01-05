afed9d52df2497e964ce166e230756d72d52a574
NeroXie <xyh30902@163.com>
2021-01-05 10:53:32 +0800

    update demo

--- Example/WKWebViewJSBridge/ViewController.swift
+++ Example/WKWebViewJSBridge/ViewController.swift
@@ -16,12 +16,24 @@ class ViewController: UIViewController {
     
     var bridge: WKWebViewJSBridge!
     
+    let tableView = UITableView(frame: .zero, style: .plain)
+    
+    var data: [String] = []
+        
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        webView.frame = view.bounds
+        webView.frame = CGRect(x: view.bounds.origin.x, y: view.bounds.origin.y, width: view.bounds.size.width, height: view.bounds.size.height * 0.5)
         webView.navigationDelegate = self
         view.addSubview(webView)
+        
+        tableView.backgroundColor = .gray
+        tableView.frame = CGRect(x: view.bounds.origin.x, y: view.bounds.size.height * 0.5, width: view.bounds.size.width, height: view.bounds.size.height * 0.5)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "\(UITableViewCell.self)")
+        tableView.rowHeight = 30;
+        view.addSubview(tableView)
         
         let width: CGFloat = (UIScreen.main.bounds.size.width - 40) * 0.5
         let y: CGFloat = UIScreen.main.bounds.size.height - 100
@@ -38,11 +50,13 @@ class ViewController: UIViewController {
         clearButton.setTitle("Clear", for: .normal)
         clearButton.addTarget(self, action: #selector(didClearButtonPressed), for: .touchUpInside)
         view.addSubview(clearButton)
-
+        
+        
         bridge = WKWebViewJSBridge(webView: webView)
-        bridge.register(handlerName: "nameFromNative") { (data, callback) in
-            print("[nameFormNative] called by JS, Data: \(data!)")
-            print("Native responding Data!")
+        bridge.register(handlerName: "nameFromNative") { [weak self] (data, callback) in
+            self?.data.append("[nameFormNative] called by JS, Data: \(data!)")
+            self?.data.append("Native responding Data!")
+            self?.tableView.reloadData()
             callback?("Nero Native")
         }
         
@@ -67,24 +81,43 @@ class ViewController: UIViewController {
     
     @objc
     private func didCallJSButtonPressed() {
-        bridge.call(handlerName: "nameFromJS", data: "Hi, JS!") { response in
-            print("Native got response: \(response!)")
+        bridge.call(handlerName: "nameFromJS", data: "Hi, JS!") { [weak self] response in
+            self?.data.append("Native got response: \(response!)")
+            self?.tableView.reloadData()
         }
     }
     
     @objc
     private func didClearButtonPressed() {
         webView.reload()
+        data.removeAll()
+        tableView.reloadData()
     }
 }
 
 extension ViewController: WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        print("webViewDidStartLoad")
+        data.append("webViewDidStartLoad")
+        tableView.reloadData()
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("webViewDidFinishLoad")
+        data.append("webViewDidFinishLoad")
+        tableView.reloadData()
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { data.count }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "\(UITableViewCell.self)")!
+        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.font = .systemFont(ofSize: 10)
+        return cell
     }
 }
